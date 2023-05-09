815c9999d485f4da4187b70ebb321eb00fe4e0ab
srimythri1018 <115878123+srimythri1018@users.noreply.github.com>
2023-05-09 22:03:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // your code here
+function myFunction()
+			{
+				let name=getElementById("name").value;
+				let year=getElementById("year").value;
+				var queryString = "?";
+                 queryString += "name=" + encodeURIComponent(name);
+                 queryString += "&year=" + encodeURIComponent(year);
+				 document.getElementById("url").textContent = "https://localhost:8080/" + queryString;
+			}
